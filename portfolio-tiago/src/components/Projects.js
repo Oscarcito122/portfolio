43eb090a26d6266181dd076b982669900010a5d8
Oscarcito122 <oscarbergmann1106@gmail.com>
2023-09-05 20:13:49 -0300

    mundo agregado a projects

--- portfolio-tiago/src/components/Projects.js
+++ portfolio-tiago/src/components/Projects.js
@@ -36,7 +36,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Proyectos</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>Todos mis proyectos han sido cuidadosamente diseñados con el objetivo de brindar una experiencia de usuario intuitiva y sencilla,Espero que disfrutes explorando lo que he creado.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     {/* <Nav.Item>
