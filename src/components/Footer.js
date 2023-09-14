28e10e32df89d65c3586b8e0ebd027ca1653851f
Oscarcito122 <oscarbergmann1106@gmail.com>
2023-09-14 20:14:33 -0300

    ahora si ultimos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import logo from "../assets/img/logo.png";
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.png';
 import navIcon3 from '../assets/img/nav-icon3.png';
-import navIcon4 from '../assets/img/nav-icon4.png';
-import Pdf from "../assets/img/TiagoCV.pdf"
+import navIcon4 from '../assets/img/nav-icon5.png';
 
 export const Footer = () => {
   return (
@@ -21,7 +20,7 @@ export const Footer = () => {
                 <a href="https://www.linkedin.com/in/tiago-bergmann-0576b7264/" target='_blank'><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/Oscarcito122" target='_blank'><img src={navIcon2} alt="" /></a>
                 <a href="https://wa.link/58wbir" target='_blank'><img src={navIcon3} alt="" /></a>
-                <a href={Pdf} target='_blank'><img src={navIcon4} alt="" /></a>
+                <a href="mailto:oscarbergmann1106@gmail.com" target='_blank'><img src={navIcon4} alt="" /></a>
 
             </div>
           </Col>
